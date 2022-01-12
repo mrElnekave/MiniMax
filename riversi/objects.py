79be93524ed8478f67060d79496acac37780c6aa
Yamm Elnekave <52299477+mrElnekave@users.noreply.github.com>
2022-01-12 09:54:46 -0800

    Delete riversi directory

--- riversi/objects.py
+++
@@ -1,5 +0,0 @@
-import pygame
-from riversi.constants import *
-
-screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
-mainClock = pygame.time.Clock()
--- riversi/try.py
+++
@@ -1,1 +0,0 @@
-print([["Cell" for j in range(4)] for i in range(4)])
